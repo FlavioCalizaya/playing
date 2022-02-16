1c1242d5f2465fd518ea7e487b17641f5125b988
FlavioCalizaya <flaviocalizayac@gmail.com>
2022-02-16 14:44:59 -0400

    third in line new feature implemented, for boxes of all sizes

--- js/board.js
+++ js/board.js
@@ -3,10 +3,13 @@ let points = [0,0];
 let piece = ["O", "X"];
 let countFill = 0;
 let endGame = false;
-let chainTripla = [[0, 1, 2], [0,3,6],[3,4,5], [6,7,8] , [0,4,8], [2,4,6], [1,4,7],[2,5,8]];
+let chainTripla = [];
+let chainNTupla = [];
+getChainNTupla(3);
 let winTripla = [];
 let countNumLimitGames = 0;
 let countNumGame = 1;
+let boardSize = 0;
 let cleanButton = document.getElementById("cleanButton");
 let newGameButton = document.getElementById("newGameButton");
 cleanButton.addEventListener("click",cleanBoard);
@@ -15,8 +18,9 @@ let numLimitGames = document.getElementById("numLimitGame");
 let numActualGame = document.getElementById("numActualGame");
 let pointPlayer1 = document.getElementById("pointPlayer1");
 let pointPlayer2 = document.getElementById("pointPlayer2");
-let boardButtons = Array.from(document.getElementsByClassName("boardButton"));
-boardButtons.forEach(x=> x.addEventListener("click",mark));
+let boardButtons = document.getElementsByClassName("boardButton");
+let boardContainer = document.getElementById("boardContainer");
+Array.from(boardButtons).forEach(x=> x.addEventListener("click",mark));
 function mark(event){
     let fixedButton = event.target;
     if(!endGame && fixedButton.innerHTML =="" ){
@@ -44,9 +48,10 @@ function changeTurn(){
 }
 function verifiyPieces(){
     let state = false;
-    chainTripla.forEach(tripla=> {
+    chainNTupla.forEach(tripla=> {
        if(boardButtons[tripla[0]].innerHTML != ""){
-        if(boardButtons[tripla[0]].innerHTML==boardButtons[tripla[1]].innerHTML && boardButtons[tripla[1]].innerHTML==boardButtons[tripla[2]].innerHTML){
+
+        if(verifyArrayEqual(tripla)){
             state = true;
             winTripla = tripla;
         }
@@ -54,6 +59,15 @@ function verifiyPieces(){
       }    
     )       
     return state;
+}
+function verifyArrayEqual(tripla){
+    let isEqual = true;
+    for(let i = 0; i< tripla.length-1; i++){
+          if(boardButtons[tripla[i]].innerHTML != boardButtons[tripla[i+1]].innerHTML){
+             isEqual = false;
+         }
+     }
+    return isEqual
 }
 function paintWinLine(){
     winTripla.forEach(index =>{
@@ -86,7 +100,7 @@ function paintWinLine(){
     }
 }
 function cleanBoard(){
- boardButtons.forEach(x => x.innerHTML = "");
+    Array.from(boardButtons).forEach(x => x.innerHTML = "");
  countFill = 0;
  numActualGame.innerHTML = countNumGame;
  endGame = false;
@@ -101,10 +115,56 @@ function setPoints(){
 }
 function newGame(){
    countNumLimitGames = prompt("input the limit matchs");
+   boardSize = prompt('input the board sice');
    numLimitGames.innerHTML = countNumLimitGames; 
    countNumGame=1 
    numActualGame.innerHTML =1;
    points=[0,0];
    setPoints();
    cleanBoard();
+   getBoard(boardSize);
+   getChainNTupla(boardSize);
+   boardButtons = document.getElementsByClassName("boardButton");
+   Array.from(boardButtons).forEach(x=> x.addEventListener("click",mark));
+}
+function getBoard(boardSize){
+    let i = 0;
+    boardContainer.innerHTML = "";
+    for(i=0; i< boardSize*boardSize; i++){
+        let buttonBoard = document.createElement("button");
+        buttonBoard.className="boardButton";
+        buttonBoard.style.width = 450/boardSize+'px';
+        buttonBoard.style.height = 450/boardSize+'px';
+        buttonBoard.style.fontSize = 3*6/boardSize +'em';
+        boardContainer.appendChild(buttonBoard);
+    }
+ }
+function getChainNTupla(boardSize){
+    chainNTupla = [];
+    let row = [];
+    let column = [];
+    let diagonal1 = [];
+    let diagonal2 = [];
+    let i = 0;
+    let j=0;
+    for(i=0 ; i<boardSize; i++){
+        diagonal1[i] = i+(boardSize)*i;
+        diagonal2[i] = (boardSize-1)+(boardSize-1)*i;
+        console.log(i);
+        console.log(boardSize);
+    }
+    console.log('dia1: '+diagonal1);
+    chainNTupla.push(diagonal1);
+    chainNTupla.push(diagonal2);
+    for(i=0;i<boardSize;i++){
+        row = [];
+        column =[];
+       for(j=0;j<boardSize;j++){
+          row[j] = i*boardSize+j;
+          column[j] = i+j*boardSize; 
+       }
+       chainNTupla.push(row);
+       chainNTupla.push(column);
+    }
+    console.log(chainNTupla);
 }
